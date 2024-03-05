30449899ea7f8d1013b3e0d9ca6e7b1f0c35e499
BennyBB97 <101838035+BennyBB97@users.noreply.github.com>
2024-03-05 20:02:26 +0100

    rename project

--- Frontend/music-storage/src/app/app.component.spec.ts
+++ Frontend/music-storage/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'radio-station' title`, () => {
+  it(`should have the 'music-storage' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('radio-station');
+    expect(app.title).toEqual('music-storage');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, radio-station');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, music-storage');
   });
 });
